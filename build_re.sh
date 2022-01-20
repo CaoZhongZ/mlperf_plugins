a35fe693564484b5a3b21aa1bf6a39090523b5c1
baodii <di.bao@intel.com>
2022-01-20 23:38:14 +0800

    step8: done release segmeng fault fixed, accuracy done

--- build_re.sh
+++ build_re.sh
@@ -1,5 +1,5 @@
 find ./ -maxdepth 1 -name "build" | xargs rm -rf
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-g" -DCMAKE_PREFIX_PATH="$(dirname $(python3 -c 'import torch; print(torch.__file__)'));../cmake/Modules" -GNinja -DUSERCP=ON ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="$(dirname $(python3 -c 'import torch; print(torch.__file__)'));../cmake/Modules" -GNinja -DUSERCP=ON ..
 ninja
 cd ..
