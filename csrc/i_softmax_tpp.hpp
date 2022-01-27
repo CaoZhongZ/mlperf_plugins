bc5c9d439a4434441cecb6ab7a4503e874aa8be4
caozhong <zhong.z.cao@intel.com>
2022-01-27 17:57:53 +0800

    Add softmax for internal shapes
    Signed-off-by: caozhong <zhong.z.cao@intel.com>

--- csrc/i_softmax_tpp.hpp
+++ csrc/i_softmax_tpp.hpp
@@ -35,6 +35,164 @@ struct i32_scale_attlen_softmax_scale_i8 {
       void *out, void *in, int32_t att_len, float M, float oscale, int64_t ld);
 };
 
+// For specific format of <int (*)[16][16]> to <int8_t (*)[16][64]>
+struct i32_scale_attlen_softmax_scale_i8_amx_tile_vnni {
+  inline static void run(
+      void *out, void *in, int32_t att_len, float M, float oscale) {
+    auto pin = reinterpret_cast<int32_t (*)[16][16]>(in);
+    auto att_tile = (att_len + 15) / 16;
+    auto att_tail = att_tile - att_len; // 1 ~ 16 possible
+
+    // Scratch for max subtraction
+    alignas(64) float dout [N][att_l16];
+
+    auto neg_large = _mm512_set1_epi32(-500000);
+    auto vscale = _mm512_set1_ps(M);
+
+    __m512 vmax[N];
+
+#   pragma unroll (16)
+    for (int i = 0; i < 16; ++i) {
+      vmax[i] = _mm512_setzero_ps();
+    }
+
+    int d2;
+    for (d2 = 0; d2 < att_tile -1; ++ d2) {
+
+#     pragma unroll (16)
+      for (int i = 0; i < 16; ++i) {
+        auto l = _mm512_loadu_si512(pin[d2][i]);
+        auto f = _mm512_cvtepi32_ps(l) * vscale;
+        vmax[i] = _mm512_max_ps(f, vmax[i]);
+        _mm512_storeu_ps(dout[d2][i], f);
+      }
+    }
+
+    __mmask16 mask = (1<<att_tail) -1;
+#   pragma unroll (16)
+    for (int i = 0; i < 16; ++i) {
+      auto l = _mm512_mask_loadu_epi32(neg_large, mask, pin[d2][i]);
+      auto f = _mm512_cvtepi32_ps(l) * vscale;
+      vmax[i] = _mm512_max_ps(f, vmax[i]);
+      _mm512_storeu_ps(dout[d2][i], f);
+    }
+
+    __m512 vsum[N];
+
+#   pragma unroll (16)
+    for (int i = 0; i < 16; ++ i) {
+      vmax[i] = _mm512_max_reduce_ps(vmax[i]);
+      vsum[i] = _mm512_setzero_ps();
+    }
+
+    for (d2 = 0; d2 < att_tile; ++ d2) {
+#     pragma unroll (16)
+      for (int i = 0; i < 16; ++ i) {
+        auto f = _mm512_loadu_ps(dout[d2][i]);
+        auto d = f - vmax[i];
+        auto e = exp_ps_0_1(d);
+        _mm512_storeu_ps(dout[d2][i], e);
+        vsum[i] += e;
+      }
+    }
+
+    auto voscale = _mm512_set1_ps(oscale);
+
+#   pragma unroll (16)
+    for (int i = 0; i < 16; ++ i) {
+#ifdef usercp
+      vsum[i] = voscale * _mm512_rcp14_ps(_mm512_add_reduce_ps(vsum[i]));
+#else
+      vsum[i] = voscale / _mm512_add_reduce_ps(vsum[i]);
+#endif
+    }
+
+    auto att_tile64 = (att_len + 63) / 64;
+    auto pout = reinterpret_cast<int8_t (*)[att_tile64][16][4][16]>(out);
+    auto zero = _mm512_setzero_ps();
+    auto att_tile16_in_tile64 = att_tile / 4;
+    auto att_tile16_in_tile64_tail = att_tile % 4;
+
+    //
+    // We have to gather 16*16 int8_t tile into 16*64 tile
+    //
+    for (d2 = 0; d2 < att_tile16_in_tile64; ++d2) {
+#     pragma unroll (16)
+      for (int i = 0; i < 16; ++i) {
+        auto l0 = _mm512_loadu_ps(dout[d2 * 4 + 0][i]);
+        auto i0 = _mm512_scale_minmax_i8_ps(l0, vsum[i]);
+
+        auto l1 = _mm512_loadu_ps(dout[d2 * 4 + 1][i]);
+        auto i1 = _mm512_scale_minmax_i8_ps(l1, vsum[i]);
+
+        auto l2 = _mm512_loadu_ps(dout[d2 * 4 + 2][i]);
+        auto i2 = _mm512_scale_minmax_i8_ps(l2, vsum[i]);
+
+        auto l3 = _mm512_loadu_ps(dout[d2 * 4 + 3][i]);
+        auto i3 = _mm512_scale_minmax_i8_ps(l3, vsum[i]);
+
+        // write combine?
+        _mm512_mask_cvtepi32_storeu_epi8(pout[d2][i][0], 0xffff, i0);
+        _mm512_mask_cvtepi32_storeu_epi8(pout[d2][i][1], 0xffff, i1);
+        _mm512_mask_cvtepi32_storeu_epi8(pout[d2][i][2], 0xffff, i2);
+        _mm512_mask_cvtepi32_storeu_epi8(pout[d2][i][3], 0xffff, i3);
+      }
+    }
+    // Tail process
+    switch (att_tile16_in_tile64_tail) {
+    case 1:
+#     pragma unroll (16)
+      for (int i = 0; i < 16; ++i) {
+        auto l0 = _mm512_loadu_ps(dout[d2 * 4 + 0][i]);
+        auto i0 = _mm512_scale_minmax_i8_ps(l0, vsum[i]);
+        auto i1 = _mm_set1_epi8(0);
+        auto i2 = _mm256_set1_epi8(0);
+
+        // write combine?
+        _mm512_mask_cvtepi32_storeu_epi8(pout[d2][i][0], 0xffff, i0);
+        _mm_storeu_epi8(pout[d2][i][1], i1);
+        _mm256_storeu_epi8(pout[d2][i][2], i2);
+      }
+      break;
+    case 2:
+#     pragma unroll (16)
+      for (int i = 0; i < 16; ++i) {
+        auto l0 = _mm512_loadu_ps(dout[d2 * 4 + 0][i]);
+        auto i0 = _mm512_scale_minmax_i8_ps(l0, vsum[i]);
+        auto l1 = _mm512_loadu_ps(dout[d2 * 4 + 1][i]);
+        auto i1 = _mm512_scale_minmax_i8_ps(l0, vsum[i]);
+        auto i2 = _mm256_set1_epi8(0);
+
+        // write combine?
+        _mm512_mask_cvtepi32_storeu_epi8(pout[d2][i][0], 0xffff, i0);
+        _mm512_mask_cvtepi32_storeu_epi8(pout[d2][i][1], 0xffff, i1);
+        _mm256_storeu_epi8(pout[d2][i][2], i2);
+      }
+      break;
+    case 3:
+#     pragma unroll (16)
+      for (int i = 0; i < 16; ++i) {
+        auto l0 = _mm512_loadu_ps(dout[d2 * 4 + 0][i]);
+        auto i0 = _mm512_scale_minmax_i8_ps(l0, vsum[i]);
+        auto l1 = _mm512_loadu_ps(dout[d2 * 4 + 1][i]);
+        auto i1 = _mm512_scale_minmax_i8_ps(l0, vsum[i]);
+        auto l2 = _mm512_loadu_ps(dout[d2 * 4 + 2][i]);
+        auto i2 = _mm512_scale_minmax_i8_ps(l0, vsum[i]);
+        auto i3 = _mm_set1_epi8(0);
+
+        // write combine?
+        _mm512_mask_cvtepi32_storeu_epi8(pout[d2][i][0], 0xffff, i0);
+        _mm512_mask_cvtepi32_storeu_epi8(pout[d2][i][1], 0xffff, i1);
+        _mm512_mask_cvtepi32_storeu_epi8(pout[d2][i][1], 0xffff, i2);
+        _mm_storeu_epi8(pout[d2][i][2], i3);
+      }
+      break;
+    default:
+      break;
+    }
+  }
+};
+
 template <int N>
 struct i32_scale_attlen_softmax_scale_i8<16, N> {
   inline static void run(
