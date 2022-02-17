0aba153fb731aa486cfd59e8c21b19c3e0ecd5f9
baodii <di.bao@intel.com>
2022-02-17 04:54:23 -0800

    add amx config

--- csrc/tpps/i_mha_tpp.cpp
+++ csrc/tpps/i_mha_tpp.cpp
@@ -6,17 +6,10 @@
 #include "i_mha_tpp.hpp"
 #include "i_softmax_tpp.hpp"
 #include "transpose.hpp"
+#include "amx_config.hpp"
 
 #include "helper.hpp"
 
-#define TMM0 0
-#define TMM1 1
-#define TMM2 2
-#define TMM3 3
-#define TMM4 4
-#define TMM5 5
-#define TMM6 6
-#define TMM7 7
 
 using Time = std::chrono::high_resolution_clock;
 namespace intel_mlperf {
@@ -24,64 +17,7 @@ namespace intel_mlperf {
 static constexpr int max_tile_row = 16;
 static constexpr int max_tile_colsb = 64;
 
-class Tilecfg {
-public:
-  void set_config(bool reconfig) const {
-    if (reconfig) {
-      _tile_release();
-      _tile_loadconfig(&cfg);
-    }
-  }
 
-  void set_config() const {
-    _tile_release();
-    _tile_loadconfig(&cfg);
-  }
-
-  Tilecfg() {
-    memset(&cfg, 0, sizeof(cfg));
-
-    for (int i = 0; i < num_valid; i++) {
-      cfg.tile_rows[i] = 16;
-      cfg.tile_colsb[i] = 64;
-    }
-    cfg.palette = 1;
-  }
-
-  Tilecfg(int k) {
-    memset(&cfg, 0, sizeof(cfg));
-
-    // static allocation
-    // A: 16 x k
-    cfg.tile_rows[4] = 16;
-    cfg.tile_colsb[4] = k * sizeof(int);
-    cfg.tile_rows[5] = 16;
-    cfg.tile_colsb[5] = k * sizeof(int);
-    // B: k x 16
-    cfg.tile_rows[6] = k;
-    cfg.tile_colsb[6] = 16 * sizeof(int);
-    cfg.tile_rows[7] = k;
-    cfg.tile_colsb[7] = 16 * sizeof(int);
-    // C: 16 x 16
-    for (int i = 0; i < 4; i++) {
-      cfg.tile_rows[i] = 16;
-      cfg.tile_colsb[i] = 16 * sizeof(int);
-    }
-    cfg.palette = 1;
-  }
-
-private:
-  static constexpr int num_valid = 8;
-  struct cfg {
-    uint8_t palette;        /* byte 0 */
-    uint8_t start_row;      /* byte 1 */
-    char rsvd1[14];         /* bytes 2-15 */
-    uint16_t tile_colsb[8]; /* bytes 16-31 */
-    char rsvd2[16];         /* bytes 32-47 */
-    uint8_t tile_rows[8];   /* bytes 48-55 */
-    char rsvd3[8];          /* bytes 56-63 */
-  } __attribute__((packed)) cfg;
-};
 
 static void reorder_k_to_buffer(int8_t *k_buffer, const int8_t *k_ptr, int row,
                                 int col_tile, int stride) {
