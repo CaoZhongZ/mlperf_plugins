1bdb3dc7fceecd00afffb1ce8f04fb3c13541aa6
baodii <di.bao@intel.com>
2022-03-02 10:30:40 +0800

    add plain format to fundamental block

--- csrc/tpps/i_linear_tpp.hpp
+++ csrc/tpps/i_linear_tpp.hpp
@@ -9,6 +9,8 @@
 
 namespace intel_mlperf {
 
+enum class load_policy {block, plain};
+
 template <int row_tile, int col_tile>
 struct _tile_dot_product_16x256 {
 
@@ -21,6 +23,14 @@ struct _tile_dot_product_16x256 {
 
     _tile_loadd(TMM4, A_[0], 64);
     if (row_tile == 2) _tile_loadd(TMM5, A_[1], 64);
+  }
+
+  inline static void load_plain_A(void *A) {
+    auto A_ = reinterpret_cast<int8_t (*)[16][col_tile][64]>(A);
+    size_t lda = col_tile * 64;
+
+    _tile_loadd(TMM4, A_[0], lda);
+    if (row_tile == 2) _tile_loadd(TMM5, A_[1], lda);
   }
 
   inline static void load_B(void *B) {
@@ -97,6 +107,49 @@ struct _tile_dot_product_16x256 {
     }
   }
 
+  inline static void quant_plain_out(void *C, void *s_0, void *s_1, float *bias, float scale, size_t ldc) {
+    auto s_0_ = reinterpret_cast<int (*)[2][16][16]>(s_0);
+    auto s_1_ = reinterpret_cast<int (*)[2][16][16]>(s_1);
+
+    auto scale_ = _mm512_set1_ps(scale);
+    constexpr size_t c_block = 16 * 64;
+    auto bias_ = reinterpret_cast<float (*)[16]>(bias);
+
+    auto b0 = _mm512_loadu_ps(bias_[0]);
+    auto b1 = _mm512_loadu_ps(bias_[1]);
+    auto b2 = _mm512_loadu_ps(bias_[2]);
+    auto b3 = _mm512_loadu_ps(bias_[3]);
+
+    auto C_ = reinterpret_cast<int8_t (*)[16][ldc]>(C);
+    #pragma unroll (row_tile)
+    for (int t = 0; t < row_tile; ++ t) {
+
+      #pragma unroll (16)
+      for (int i = 0; i < 16; ++ i) {
+        auto i0 = _mm512_load_epi32(s_0_[t][0][i]);
+        auto i1 = _mm512_load_epi32(s_0_[t][1][i]);
+        auto i2 = _mm512_load_epi32(s_1_[t][0][i]);
+        auto i3 = _mm512_load_epi32(s_1_[t][1][i]);
+
+        auto f0 = _mm512_cvtepi32_ps(i0) + b0;
+        auto f1 = _mm512_cvtepi32_ps(i1) + b1;
+        auto f2 = _mm512_cvtepi32_ps(i2) + b2;
+        auto f3 = _mm512_cvtepi32_ps(i3) + b3;
+
+        auto o0 = _mm512_scale_minmax_i8_ps(scale_, f0);
+        auto o1 = _mm512_scale_minmax_i8_ps(scale_, f1);
+        auto o2 = _mm512_scale_minmax_i8_ps(scale_, f2);
+        auto o3 = _mm512_scale_minmax_i8_ps(scale_, f3);
+
+        // every 16 got output
+        _mm512_mask_cvtepi32_storeu_epi8(&C_[t][i][0], 0xffff, o0);
+        _mm512_mask_cvtepi32_storeu_epi8(&C_[t][i][16], 0xffff, o1);
+        _mm512_mask_cvtepi32_storeu_epi8(&C_[t][i][32], 0xffff, o2);
+        _mm512_mask_cvtepi32_storeu_epi8(&C_[t][i][48], 0xffff, o3);
+      }
+    }
+  }
+
   // Pure tile format
   inline static void compute(void *C, void *A, void *B, float *bias, float scale) {
     alignas (64) int scratch_0[row_tile][2][16][16];
@@ -126,6 +179,36 @@ struct _tile_dot_product_16x256 {
 
     store(scratch_1);
     quant_out(C, scratch_0, scratch_1, bias, scale);
+  }
+
+  inline static void plain_compute(void *C, void *A, void *B, float *bias, float scale, size_t ldc) {
+    alignas (64) int scratch_0[row_tile][2][16][16];
+    alignas (64) int scratch_1[row_tile][2][16][16];
+
+    auto A_ = reinterpret_cast<int8_t (*)[col_tile][64]>(A);
+    auto B_ = reinterpret_cast<int8_t (*)[col_tile][16][64]>(B);
+
+    zero_accum();
+
+#   pragma unroll (col_tile)
+    for (int i = 0; i < col_tile; ++i) {
+      load_plain_A(A_[0][i]);
+      load_B(B_[0][i]);
+      dot_prod();
+    }
+
+    store(scratch_0);
+    zero_accum();
+
+#   pragma unroll (col_tile)
+    for (int i = 0; i < col_tile; ++i) {
+      load_plain_A(A_[0][i]);
+      load_B(B_[2][i]);
+      dot_prod();
+    }
+
+    store(scratch_1);
+    quant_plain_out(C, scratch_0, scratch_1, bias, scale, ldc);
   }
 };
 
--- csrc/tpps/main.cpp
+++ csrc/tpps/main.cpp
@@ -53,7 +53,7 @@ void set_data_wei(void *w, void* b) {
   }
 }
 
-void test_accuracy_linear(int row_tile);
+void test_accuracy_linear(int row_tile, bool is_block = true);
 
 static constexpr size_t cols = 384;
 static constexpr size_t rows = 16;
@@ -67,7 +67,7 @@ int main(int argc, char* argv[]) {
   }
   std::cout << "++++++++++++++++++++++++++ row_tile = "<< row_tile << " ++++++++++++++++++++++++++++++++++++" << std::endl;
   
-  test_accuracy_linear(row_tile);
+  test_accuracy_linear(row_tile, false);
   return 0;
 }
 
@@ -105,19 +105,29 @@ void naive_linear(void* a, void* b, void* c, void* bias, float scale, int row_ti
   }
 }
 
-void send_data2naive(void* a, void* b, void* na, void* nb, int row_tile) {
-  auto a_ = reinterpret_cast<int8_t (*)[16][16][64]>(a);
-  auto na_ = reinterpret_cast<int (*)[1024]>(na);
-
-  for (int i = 0; i < row_tile; i++) {
-    for (int j = 0; j < 16; j++) {
-      for (int k = 0; k < 16; k++) {
-        for (int m = 0; m < 64; m++) {
-          na_[i * 16 + k][j * 64 + m] = static_cast<int>(a_[i][j][k][m]);
+void send_data2naive(void* a, void* b, void* na, void* nb, int row_tile, bool is_block) {
+  if (is_block) {
+    auto a_ = reinterpret_cast<int8_t (*)[16][16][64]>(a);
+    auto na_ = reinterpret_cast<int (*)[1024]>(na);
+    for (int i = 0; i < row_tile; i++) {
+      for (int j = 0; j < 16; j++) {
+        for (int k = 0; k < 16; k++) {
+          for (int m = 0; m < 64; m++) {
+            na_[i * 16 + k][j * 64 + m] = static_cast<int>(a_[i][j][k][m]);
+          }
         }
       }
     }
+  } else {
+    auto a_ = reinterpret_cast<int8_t (*)[1024]>(a);
+    auto na_ = reinterpret_cast<int (*)[1024]>(na);
+    for (int i = 0; i < row_tile * 16; i++) {
+      for (int j = 0; j < 1024; j++) {
+        na_[i][j] = static_cast<int>(a_[i][j]);
+      }
+    }
   }
+  
 
   auto b_ = reinterpret_cast<int8_t (*)[16][16][64]>(b);
   auto nb_ = reinterpret_cast<int (*)[64]>(nb);
@@ -135,7 +145,7 @@ void send_data2naive(void* a, void* b, void* na, void* nb, int row_tile) {
   }
 }
 
-void test_accuracy_linear(int row_tile) {
+void test_accuracy_linear(int row_tile, bool is_block) {
   alignas(64) int8_t act[row_tile * 16 * 1024];
   alignas(64) int8_t wei[256 * 256];
   alignas(64) int nact[row_tile * 16 * 1024];
@@ -148,7 +158,7 @@ void test_accuracy_linear(int row_tile) {
   set_data_act(act, row_tile);
   set_data_wei(wei, bias);
 
-  send_data2naive(act, wei, nact, nwei, row_tile);
+  send_data2naive(act, wei, nact, nwei, row_tile, is_block);
   auto act_ = reinterpret_cast<int8_t (*)[16][16][64]>(act);
   auto nact_ = reinterpret_cast<int (*)[1024]>(nact);
   auto wei_ = reinterpret_cast<int8_t (*)[16][16][64]>(wei);
@@ -160,7 +170,11 @@ void test_accuracy_linear(int row_tile) {
   naive_linear(nact, nwei, nout, bias, scale, row_tile);
   switch (row_tile) {
   case (2):
-    intel_mlperf::_tile_dot_product_16x256<2, 16>::compute(out, act, wei, bias, scale);
+    if (is_block) {
+      intel_mlperf::_tile_dot_product_16x256<2, 16>::compute(out, act, wei, bias, scale);
+    } else {
+      intel_mlperf::_tile_dot_product_16x256<2, 16>::plain_compute(out, act, wei, bias, scale, 64);
+    }
     break;
   case (3):
     intel_mlperf::_tile_dot_product_16x256<3, 16>::compute(out, act, wei, bias, scale);
