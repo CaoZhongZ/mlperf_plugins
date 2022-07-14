7a09569b54a3f200a60b5bdc74cfecc3cae679a2
baodii <di.bao@intel.com>
2022-07-13 20:40:22 -0700

    fix mha cat interface

--- csrc/amx_mha_concat.cpp
+++ csrc/amx_mha_concat.cpp
@@ -13,7 +13,7 @@
 
 namespace intel_mlperf {
 
-at::Tensor amx_mha_concat(const at::Tensor &qkv, const at::Tensor &att_mask,
+at::Tensor amx_mha_concat(const at::Tensor &qkv, const at::Tensor &att_mask, const at::Tensor& length_ids,
                           const at::Scalar &m1, const at::Scalar &oscale,
                           const at::Scalar &m2) {
   auto qkv_sizes = qkv.sizes();
@@ -22,7 +22,7 @@ at::Tensor amx_mha_concat(const at::Tensor &qkv, const at::Tensor &att_mask,
   assert(bs == 1);
   auto sl = qkv_sizes[1];
   auto stride = qkv_sizes[2];
-  auto real_bs = att_mask.sizes()[0] - 1;
+  auto real_bs = length_ids.sizes()[0] - 1;
 
   auto qkv_block = stride / 3;
   int head_size = 64;
@@ -36,6 +36,7 @@ at::Tensor amx_mha_concat(const at::Tensor &qkv, const at::Tensor &att_mask,
   auto att_data_ptr = attention.data_ptr();
   auto qkv_data_ptr = qkv.data_ptr();
   auto att_mask_p = reinterpret_cast<int32_t *>(att_mask.data_ptr());
+  auto length_p = reinterpret_cast<int32_t *>(length_ids.data_ptr());
 
   auto m1_ = m1.toFloat();
   auto m2_ = m2.toFloat();
@@ -46,11 +47,12 @@ at::Tensor amx_mha_concat(const at::Tensor &qkv, const at::Tensor &att_mask,
 #pragma omp parallel for collapse(2)
   for (int i = 0; i < real_bs; i++) {         // batch size
     for (int j = 0; j < head_num; j++) { // head num
-      auto sl = att_mask_p[i + 1] - att_mask_p[i];
+      auto sl = length_p[i + 1] - length_p[i];
+
       auto att_ptr = reinterpret_cast<int8_t(*)[head_num][head_size]>(att_data_ptr);
       auto origin_ptr = reinterpret_cast<int8_t(*)[3][head_num][head_size]>(qkv_data_ptr);
-      auto cur_q_ptr = origin_ptr[att_mask_p[i]][0][j];
-      auto cur_a_ptr = att_ptr[att_mask_p[i]][j];
+      auto cur_q_ptr = origin_ptr[length_p[i]][0][j];
+      auto cur_a_ptr = att_ptr[length_p[i]][j];
 
       // auto total_core_num = omp_get_num_threads();
       // auto core_id = omp_get_thread_num();
@@ -61,7 +63,7 @@ at::Tensor amx_mha_concat(const at::Tensor &qkv, const at::Tensor &att_mask,
       // }
 
       amx_per_head(cur_q_ptr, stride, cur_a_ptr, qkv_block, sl, m1_,
-                   oscale_, sl, m2_);
+                   oscale_, att_mask_p[i], m2_);
     }
   }
   return attention;
--- csrc/amx_mha_concat.hpp
+++ csrc/amx_mha_concat.hpp
@@ -6,6 +6,7 @@ namespace intel_mlperf {
 at::Tensor amx_mha_concat(
     const at::Tensor& qkv,
     const at::Tensor& att_mask,
+    const at::Tensor& length_ids,
     const at::Scalar& m1,
     const at::Scalar& oscale,
     const at::Scalar& m2
