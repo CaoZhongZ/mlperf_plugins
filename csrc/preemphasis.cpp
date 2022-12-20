3a9913feb5275eb556d6e24472465ffabf9e63ed
Chen, Zhenhuan <zhenhuan.chen@intel.com>
2022-12-20 01:02:13 -0800

    fuse padding for stft in preemphasis

--- csrc/preemphasis.cpp
+++ csrc/preemphasis.cpp
@@ -1,24 +1,28 @@
 #include "preemphasis.hpp"
-#include "tpps/preemphasis_tpp.hpp"
+
 #include <c10/core/MemoryFormat.h>
 #include <c10/core/ScalarType.h>
 #include <c10/core/TensorOptions.h>
 
+#include "tpps/preemphasis_tpp.hpp"
+
 namespace intel_mlperf {
-at::Tensor preemphasis(const at::Tensor &input,
-                       const c10::optional<at::Scalar> &coeff) {
+at::Tensor preemphasis(
+    const at::Tensor &input, const at::optional<at::Scalar> &coeff,
+    const at::optional<at::Scalar> &pad_size) {
   if (!input.is_contiguous()) {
     throw std::runtime_error("Input should be contiguous.");
   }
   auto in_sz = input.sizes();
-  auto output =
-      at::empty(in_sz, at::TensorOptions().dtype<float>().memory_format(
-                           c10::MemoryFormat::Contiguous));
   auto batch = in_sz[0];
-  int64_t seq_len = in_sz[1];
+  auto seq_len = in_sz[1];
+  auto pad_size_ = pad_size.value_or(0).toInt();
+  auto output = at::zeros(
+      {batch, seq_len + 2 * pad_size_},
+      at::TensorOptions().dtype<float>().memory_format(c10::MemoryFormat::Contiguous));
 
-  auto *src = static_cast<float *>(input.data_ptr());
-  auto *dst = static_cast<float *>(output.data_ptr());
+  auto src = input.accessor<float, 2>();
+  auto dst = output.accessor<float, 2>();
   auto coeff_ = coeff.value_or(0.97f).toFloat();
 
   if (coeff_ == 0.0f) {
@@ -26,12 +30,11 @@ at::Tensor preemphasis(const at::Tensor &input,
   } else {
 #pragma omp parallel for
     for (auto i = 0; i < batch; i++) {
-      preemphasis_tpp::ref(&dst[i * seq_len], &src[i * seq_len], seq_len,
-                           coeff_);
+      preemphasis_tpp::ref(&dst[i][pad_size_], &src[i][0], seq_len, coeff_);
     }
   }
 
   return output;
 }
 
-} // namespace intel_mlperf
+}  // namespace intel_mlperf
